3474aca60430a5dc2afc8c0979327fb93df6fa26
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-02-07 16:59:27 +0700

    create tags-input component

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,3 +1,24 @@
 <template>
-    <input type="text"/>
+    <!-- directive -->
+    <!-- <div v-text="tags"></div> -->
+    <div v-if="!tags.length">You have no any tags</div>
+    <div v-for="tag in tags">{{ tag }}</div>
+
+    <hr>
+
+    <!-- interpolation -->
+    <div>{{ tags }}</div>
+
+    <input type="text" v-bind:value="newTag" value="nuxt"/>
+
+    <!-- <div>{{ tags.length }}</div> -->
 </template>
+
+<script>
+export default {
+    data: () => ({
+        tags: ["Vue", "React", "Angular"],
+        newTag: "Next"
+    })
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import TagsInput from './components/TagsInput.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// Register the TagsInput component globally
+// Receive two parameters: the name of the component and the component itself
+app.component('tags-input', TagsInput)
+app.mount('#app')
